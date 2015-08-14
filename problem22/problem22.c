a73e95d4e1b27de85e6f4edcdb37b24c9a77c28d
Brent Bovenzi <brent@Brents-MacBook-Pro.local>
2015-08-14 15:25:08 -0400

    removed hardcoding of input size

--- problem22/problem22.c
+++ problem22/problem22.c
@@ -2,50 +2,71 @@
 #include <stdlib.h>
 #include <string.h>
 
+int length = 1;
 
 int compare (const void * a, const void * b ) {
   return strcmp(*(char **)a, *(char **)b);
 }
 
-int main()
-{
-	int totalsum = 0;
-	int charsum = 0;
-   char namelist[47000];
-   FILE * fp;
-   int i = 0;
-   int j = 0;
-   int size = 5163;
-   char *names[size];
+char *inputString(FILE* fp, size_t size){
+//The size is extended by the input with the value of the provisional
+    char *str;
+    int ch;
+    size_t len = 0;
+    str = realloc(NULL, sizeof(char)*size);//size is start size
+    if(!str)return str;
+    while(EOF!=(ch=fgetc(fp)) && ch != '\n'){
+        if(ch == ','){
+            length++;
+        }
+        if(ch != '\"'){
+            str[len++]=ch;
+        }
+        if(len==size){
+            str = realloc(str, sizeof(char)*(size+=16));
+            if(!str)return str;
+        }
+    }
+    str[len++]='\0';
+
+    return realloc(str, sizeof(char)*len);
+}
+
+int main(void){
+
+    char *namelist;
+    int totalsum = 0;
+    int charsum = 0;
+    int i = 0;
+    int j;
+    char* buffer;
+
+    FILE* fp = fopen ("names.txt", "r");
+    namelist = inputString(fp, 10);
+
+    char *names[length];
+
+    buffer = strtok (namelist, ",");
+    while (buffer) {
+        if( i >= length)
+            break;
+        names[i] = buffer;
+        i++;
+        buffer = strtok (NULL, ",");
+    }
+
+    size_t strings_len = sizeof(names) / sizeof(char *);
+    qsort(names, strings_len, sizeof(char *), &compare);
 
 
-   fp = fopen ("names.txt", "r");
-   fscanf(fp, "%s", namelist);
-   
-   char* buffer;
-   buffer = strtok (namelist, "\",\"");
-   while (buffer) {
-   		if( i >= size)
-   			break;
-		names[i] = buffer;
-		i++;
-	    buffer = strtok (NULL, "\",\"");
-	    while (buffer && *buffer == '\040')
-	        buffer++;
-   }
-   fclose(fp);
 
-   size_t strings_len = sizeof(names) / sizeof(char *);
-   qsort(names, strings_len, sizeof(char *), &compare);
-
-   for(i = 0; i < size; i++){
-   		charsum = 0;
-   		for( j = 0; j < strlen(names[i]); j++){
-   			charsum+= (int) names[i][j] - 64;
-   		}
-   		totalsum+= charsum * (i+1);
-   }
-
-   printf("%d\n", totalsum);
-   return(0);
+    for(i = 0; i < length; i++){
+        charsum = 0;
+        for( j = 0; j < strlen(names[i]); j++){
+            charsum+= (int) names[i][j] - 64;
+        }
+        totalsum+= charsum * (i+1);
+    }
+    printf("%d\n", totalsum);
+    return 0;
 }
